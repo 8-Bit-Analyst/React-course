0b83ec8fa4cca5fc9fab73211a92b66e1ea86c53
8-Bit-Analyst <metaanalysisf@gmail.com>
2022-05-18 17:53:42 +1000

    child to parent communication
    starting to begin to send data from child to parent

--- complete-react-guide/src/components/Expenses/NewExpense/ExpenseForm.js
+++ complete-react-guide/src/components/Expenses/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./NewExpense.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 	const [enteredTitle, setEnteredTitle] = useState("");
 	const [enteredAmount, setEnteredAmount] = useState("");
 	const [enteredDate, setEnteredDate] = useState("");
@@ -26,7 +26,10 @@ const ExpenseForm = () => {
 			amount: enteredAmount,
 			date: new Date(enteredDate),
 		};
-		console.log(expenseData);
+		props.onSaveExpenseData(expenseData);
+		setEnteredTitle("");
+		setEnteredAmount("");
+		setEnteredDate("");
 	};
 
 	return (
@@ -34,12 +37,17 @@ const ExpenseForm = () => {
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" onChange={titleChangeHandler} />
+					<input
+						type="text"
+						value={enteredTitle}
+						onChange={titleChangeHandler}
+					/>
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
 					<input
 						type="number"
+						value={enteredAmount}
 						min="0.01"
 						step="0.01"
 						onChange={amountChangeHandler}
@@ -49,8 +57,9 @@ const ExpenseForm = () => {
 					<label>Date</label>
 					<input
 						type="date"
-						min="2019-01-01"
-						max="2022-12-31"
+						value={enteredDate}
+						min="01-01-2019"
+						max="31-12-2022"
 						onChange={dateChangeHandler}
 					/>
 				</div>
--- complete-react-guide/src/components/Expenses/NewExpense/NewExpense.js
+++ complete-react-guide/src/components/Expenses/NewExpense/NewExpense.js
@@ -2,9 +2,17 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = () => {
+	const saveExpenseDataHandler = (enteredExpenseData) => {
+		const expenseData = {
+			...enteredExpenseData,
+			id: Math.random().toString(),
+		};
+		console.log(expenseData);
+	};
+
 	return (
 		<div className="new-expense">
-			<ExpenseForm />
+			<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
 		</div>
 	);
 };
