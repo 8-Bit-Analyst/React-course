257aae6b086be7ec166dd38c320ef8224fe17c35
8-Bit-Analyst <metaanalysisf@gmail.com>
2022-05-18 14:22:38 +1000

    Changed the way ueState was formatted

--- complete-react-guide/src/components/Expenses/NewExpense/ExpenseForm.js
+++ complete-react-guide/src/components/Expenses/NewExpense/ExpenseForm.js
@@ -2,20 +2,31 @@ import React, { useState } from "react";
 import "./NewExpense.css";
 
 const ExpenseForm = () => {
-	const [enteredTitle, setEnteredTitle] = useState("");
-	const [enteredAmount, setEnteredAmount] = useState("");
-	const [enteredDate, setEnteredDate] = useState("");
+	const [userInput, setUserInput] = useState({
+		enteredTitle: "",
+		enteredAmount: "",
+		enteredDate: "",
+	});
 
 	const titleChangeHandler = (event) => {
-		setEnteredTitle(event.target.value);
+		setUserInput({
+			...userInput,
+			enteredTitle: event.target.value,
+		});
 	};
 
 	const amountChangeHandler = (event) => {
-		setEnteredAmount(event.target.value);
+		setUserInput({
+			...userInput,
+			enteredAmount: event.target.value,
+		});
 	};
 
 	const dateChangeHandler = (event) => {
-		setEnteredDate(event.target.event);
+		setUserInput({
+			...userInput,
+			enteredDate: event.target.value,
+		});
 	};
 
 	return (
