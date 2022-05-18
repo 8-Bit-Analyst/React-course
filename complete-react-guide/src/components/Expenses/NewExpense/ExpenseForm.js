ad074404a820b16065b1a0e9155a7e87f8ae5da8
8-Bit-Analyst <metaanalysisf@gmail.com>
2022-05-18 16:46:32 +1000

    made useState more efficient

--- complete-react-guide/src/components/Expenses/NewExpense/ExpenseForm.js
+++ complete-react-guide/src/components/Expenses/NewExpense/ExpenseForm.js
@@ -8,25 +8,15 @@ const ExpenseForm = () => {
 		enteredDate: "",
 	});
 
-	const titleChangeHandler = (event) => {
+	const changeHandler = (event) => {
 		setUserInput({
 			...userInput,
-			enteredTitle: event.target.value,
+			[event.target.name]: event.target.value,
 		});
 	};
 
-	const amountChangeHandler = (event) => {
-		setUserInput({
-			...userInput,
-			enteredAmount: event.target.value,
-		});
-	};
-
-	const dateChangeHandler = (event) => {
-		setUserInput({
-			...userInput,
-			enteredDate: event.target.value,
-		});
+	const submitHandler = (event) => {
+		event.preventDefault();
 	};
 
 	return (
@@ -34,7 +24,7 @@ const ExpenseForm = () => {
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" onChange={titleChangeHandler} />
+					<input type="text" onChange={changeHandler} />
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
@@ -42,7 +32,7 @@ const ExpenseForm = () => {
 						type="number"
 						min="0.01"
 						step="0.01"
-						onChange={amountChangeHandler}
+						onChange={changeHandler}
 					/>
 				</div>
 				<div className="new-expense__control">
@@ -51,7 +41,7 @@ const ExpenseForm = () => {
 						type="date"
 						min="2019-01-01"
 						max="2022-12-31"
-						onChange={dateChangeHandler}
+						onChange={changeHandler}
 					/>
 				</div>
 			</div>
