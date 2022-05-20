383f8f9fdf54769dae6c481b83d2fa8cc455a7c3
8-Bit-Analyst <metaanalysisf@gmail.com>
2022-05-20 11:05:19 +1000

    commented out title change button in ExpenseItem.js
    Also added style for title change button

--- complete-react-guide/src/components/Expenses/ExpenseItem.js
+++ complete-react-guide/src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,26 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "./UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-	const [title, setTitle] = useState(props.title);
+	// const [title, setTitle] = useState(props.title);
 
-	const buttonHandler = () => {
-		setTitle("Updated!");
-		console.log(title);
-	};
+	// const buttonHandler = () => {
+	// 	setTitle("Updated!");
+	// 	console.log(title);
+	// };
 
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
-				<h2>{title}</h2>
+				<h2>{props.title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
-				<button onClick={buttonHandler}>Change Title</button>
+				{/* <button className="title-button" onClick={buttonHandler}>
+					Change Title
+				</button> */}
 			</div>
 		</Card>
 	);
